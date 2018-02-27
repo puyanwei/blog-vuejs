73e4d4461856da1a70793abd83814d1e9e57d951
Puyan Wei <puyanwei@gmail.com>
2018-02-27 15:19:29 +0000

    created form to enter blog title and contents

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="add-blog">
       <h2>Add a New Blog Post</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required>
+        <br>
+        <br>
+        <label>Blog Content:</label>
+        <textarea></textarea>
+      </form>
     </div>
 </template>
 
