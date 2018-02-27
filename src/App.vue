d302b55561e24416b59e0e41abea654f48a1cb41
Puyan Wei <puyanwei@gmail.com>
2018-02-27 15:11:41 +0000

    stripped out previous code from previous tutorial, added in styling for body

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
     <div>
-      <keep-alive>
-      <component :is="component"></component>
-      </keep-alive>
-      <button @click="component = 'form-one'">Show Form One</button></button>
-      <button @click="component = 'form-two'">Show Form Two</button></button>
+    
     </div>
 </template>
 
 <script>
-import formOne from "./components/formOne.vue";
-import formTwo from "./components/formTwo.vue";
-
 export default {
-  components: {
-    "form-one": formOne,
-    "form-two": formTwo
-  },
   data() {
     return {
       component: "form-two"
@@ -27,5 +16,8 @@ export default {
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
 </style>
