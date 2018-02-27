a6fd60b00471fa55bccae8888a059d0d4d9da9e3
Puyan Wei <puyanwei@gmail.com>
2018-02-27 15:08:37 +0000

    correct files copied, added gitignore

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <keep-alive>
+      <component :is="component"></component>
+      </keep-alive>
+      <button @click="component = 'form-one'">Show Form One</button></button>
+      <button @click="component = 'form-two'">Show Form Two</button></button>
+    </div>
+</template>
+
+<script>
+import formOne from "./components/formOne.vue";
+import formTwo from "./components/formTwo.vue";
+
+export default {
+  components: {
+    "form-one": formOne,
+    "form-two": formTwo
+  },
+  data() {
+    return {
+      component: "form-two"
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
