135fe23fb64aa1c0256acf1f711dd85ba254f416
Puyan Wei <puyanwei@gmail.com>
2018-02-27 16:29:03 +0000

    retrieved data from get request and output it to the page

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: []
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      });
+  }
+};
+</script>
+
+<style>
+
+</style>
