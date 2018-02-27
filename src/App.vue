4124aeb774bc8e93bf3da34ad8800ddd5f4f6947
Puyan Wei <puyanwei@gmail.com>
2018-02-27 15:17:17 +0000

    added addBlog.vue, imported into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-    
+      <add-blog></add-blog>
     </div>
 </template>
 
 <script>
+import addBlog from "./components/addBlog.vue";
+
 export default {
+  components: {
+    "add-blog": addBlog
+  },
   data() {
-    return {
-      component: "form-two"
-    };
+    return {};
   },
   methods: {}
 };
---
+++ src/components/addBlog.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="add-blog">
+      <h2>Add a New Blog Post</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
