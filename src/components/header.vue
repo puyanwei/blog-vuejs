273eb44dbd34a381f6b8a0006139114840fd6a90
Puyan Wei <puyanwei@gmail.com>
2018-02-28 14:11:29 +0000

    created header component, used tag router-link in the template

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link :to="/">Blog</router-link></li>
+      <li><router-link :to="/add">Add a New Post</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
