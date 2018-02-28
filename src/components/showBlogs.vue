7fbfea918156304f95dac6d964e0082ca2158627
Puyan Wei <puyanwei@gmail.com>
2018-02-28 14:52:06 +0000

    added links to titles of blogs in show blogs page

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,7 +3,7 @@
     <h1>All Blog Articles</h1>
     <input type="text" v-model="search" placeholder="search posts">
     <div v-for="blog in filteredBlogs" class="single-blog">
-      <h2 v-multicolor>{{ blog.title | to-uppercase }}</h2>
+      <router-link :to="'/blog/' + blog.id"><h2 v-multicolor>{{ blog.title | to-uppercase }}</h2></router-link>
       <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
