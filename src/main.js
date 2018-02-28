dee3ac606cb53826a353a8a1cf9ac754b83c6366
Puyan Wei <puyanwei@gmail.com>
2018-02-28 13:15:50 +0000

    installed vue-router, imported into main.js and created new instance of it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue"; // component template imported
 import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({});
 
 // Custom filters
 
