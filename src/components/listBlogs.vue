0cc61f82a56a615824004feb2e3fdba934df7010
Puyan Wei <puyanwei@gmail.com>
2018-02-28 12:56:59 +0000

    added new component listBlogs that only lists the titles and not the contents

---
+++ src/components/listBlogs.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+    <h1>List Blog Titles</h1>
+    <input type="text" v-model="search" placeholder="search posts">
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2 v-multicolor>{{ blog.title | to-uppercase }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: ""
+    };
+  },
+  methods: {},
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search);
+      });
+    }
+  },
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      });
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  directives: {
+    multicolor: {
+      bind(el, binding, vnode) {
+        el.style.color =
+          "#" +
+          Math.random()
+            .toString()
+            .slice(2, 8);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0px auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
