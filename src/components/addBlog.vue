26a6a63b62cc889c1560e27f580c0d30284f30ad
Puyan Wei <puyanwei@gmail.com>
2018-02-28 15:04:51 +0000

    setup firebase, changed post request url to firebase and set up to send whole blog hash

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -56,11 +56,7 @@ export default {
   methods: {
     post: function() {
       this.$http
-        .post("http://jsonplaceholder.typicode.com/posts", {
-          title: this.blog.title,
-          body: this.blog.content,
-          userId: 1
-        })
+        .post("https://vue-js-blog-6bc85.firebaseio.com/posts.json", this.blog)
         .then(function(data) {
           console.log(data);
           this.submitted = true;
